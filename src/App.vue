03f06eada5b72467300c07737af23b472125c799
so young <sybang@telelian.com>
2019-05-21 13:26:38 +0900

    third web

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
-<script>
-  import MainLayout from '@/components/MainLayout';
-
-  export default {
-    name: 'app',
-    components: { MainLayout },
-    data: () => {
-      return {};
-    },
-  };
-</script>
-
 <template>
-  <div id="app">
-    <MainLayout></MainLayout>
-  </div>
+  <v-app>
+    <v-content>
+      <v-row class="hidden-lg-and-up">
+        <v-img :src="require('./assets/mobile.jpg')"/>
+      </v-row>
+      <v-row class="hidden-sm-and-down">
+        <v-img :src="require('./assets/web.jpg')"/>
+      </v-row>
+    </v-content> 
+    <v-footer height="auto" width = "100%" class="white pa-3 app--footer">
+      <v-card flat tile class="flex text-xs-center">  
+        <v-card-text class="pt-0">
+          1342, Seongnam-daero, Sujeong-gu, Seongnam-si, Gyeonggi-do, Korea<br/>
+          gudokim@telelian.com
+        </v-card-text>  
+        <v-divider></v-divider>  
+        <v-card-text>
+          &copy;2018 — <strong>Telelian</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>   
+  </v-app>
 </template>
 
+<script>
 
-<style>
-</style>
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,15 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import App from './App';
-import router from './router.js';
-import store from './store';
+import 'es6-promise/auto'
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css' 
+import './theme/default.styl'
 
-import('./assets/main.styl');
-import('babel-polyfill');
+Vue.use(Vuetify)
 
-Vue.config.productionTip = false;
-Vue.use(Vuetify);
+Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App },
-  store: () => store,
-});
+  render: h => h(App),
+}).$mount('#app')
